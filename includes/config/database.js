9770afb09baa2267a45352ea7b0ab5d67ea625fa
Julian David Sandoval Godinez <76192811+Julian-Sandoval-x@users.noreply.github.com>
2024-12-08 16:34:31 -0800

    Refactorizacion de conexion a DB

--- includes/config/database.js
+++ includes/config/database.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
-import { createConnection, createPool } from "mysql2";
+import { createPool } from "mysql2";
 
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
+// Crear el pool de conexiones
 const db = createPool({
-  host: "autorack.proxy.rlwy.net",
+  host: "autorack.proxy.rlwy.net", // Usa variables de entorno para flexibilidad
   user: "root",
   password: "fkPYZOChHznaSqGQwokzTKorSBJfPMIi",
   database: "railway",
@@ -15,12 +16,15 @@ const db = createPool({
   connectTimeout: 10000, // 10 segundos
 });
 
-db.connect((err) => {
+// Verificar la conexión al inicializar
+db.getConnection((err, connection) => {
   if (err) {
-    console.error("Error conectando a la base de datos:", err);
+    console.error("Error al conectar a la base de datos:", err);
     return;
   }
-  console.log("Conectado a la base de datos");
+  console.log("Conexión exitosa a la base de datos");
+  connection.release(); // Libera la conexión inmediatamente
 });
 
+// Exportar el pool para su uso en el proyecto
 export default db;
